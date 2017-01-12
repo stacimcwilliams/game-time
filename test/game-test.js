21dbdb2c32faa5e075f1e7fa020710d56ed9b2c4
Staci McWilliams <stacinicole86@gmail.com>
2017-01-12 08:37:56 -0700

    break canvas out of the game file so we can move forward with testing. A few tests passing in the game file testing methods.

--- test/game-test.js
+++ test/game-test.js
@@ -1,18 +1,46 @@
-// const chai = require('chai');
-// const assert = chai.assert;
-//
-// const Game = require('../lib/game');
-// const Ball = require('../lib/ball');
-// const Paddle = require('../lib/paddle');
-// const Tile = require('../lib/tile');
-//
-// describe('Game', function() {
-//     context('default', function() {
-//       var game = new Gall({
-//
-//       })
-//   it('should have a function "game"', function() {
-//     assert.isFunction(Game);
-//   })
-//     })
-// })
+const chai = require('chai');
+const assert = chai.assert;
+
+const Game = require('../lib/game');
+const Ball = require('../lib/ball');
+const Paddle = require('../lib/paddle');
+const Tile = require('../lib/tile');
+
+describe('Game', function() {
+    context('default', function() {
+      var game = new Game({
+        context: {},
+        canvas: {width:500},
+        width: 10,
+        height: 10,
+        y: 10,
+      });
+  it('should have a function "game"', function() {
+    assert.isFunction(Game);
+  });
+
+  it('should instantiate Game', function() {
+    assert.isObject(game);
+  });
+
+  it('should have a method called "buildGame"', function() {
+    assert.isFunction(game.buildGame);
+  });
+
+  it('should have a method called "makeTileArray"', function() {
+    assert.isFunction(game.makeTileArray);
+  });
+
+  // it('"makeTileArray" should return an array', function() {
+  //   assert.isArray(game.makeTileArray);
+  // });
+
+  it('should have a method called ballTileCollision', function() {
+    assert.isFunction(game.ballTileCollision);
+  });
+
+  it('"ballTileCollision" should reverse the directionY', function() {
+    assert.equal(game.y, -10);
+  })
+});
+});
